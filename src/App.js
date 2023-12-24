755045b7bfd6053efb2d5a988cb3e4fcca3cafb5
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2023-12-24 22:24:19 +0100

    frontend codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useHistory,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
+import Navbar from "./components/Navbar";
+import Conversations from "./pages/Conversations";
+import Messages from "./pages/Messages";
 function App() {
   return (
-    <div className="">
-      <header className="text-2xl bg-red-600">
-        come
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={auth ? <Social /> : <Login />} /> */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/conversations" element={<Conversations />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-image': "url('/background.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
   },
   plugins: [],
 }
